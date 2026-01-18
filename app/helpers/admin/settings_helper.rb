f6c59a3aaa06e57b99fd75516f88ff42b3f4c263
Ale Bardin <alejandro.bardin01@gmail.com>
2026-01-17 23:31:35 -0300

    feat: add Spanish translations for settings labels

--- app/helpers/admin/settings_helper.rb
+++ app/helpers/admin/settings_helper.rb
@@ -1,2 +1,11 @@
 module Admin::SettingsHelper
+  def setting_label(key)
+    labels = {
+      "whatsapp_number" => "Número de WhatsApp",
+      "facebook_pixel" => "Facebook Pixel ID",
+      "hero_title" => "Título del Hero",
+      "hero_subtitle" => "Subtítulo del Hero"
+    }
+    labels[key] || key.humanize
+  end
 end
